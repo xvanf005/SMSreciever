572114945b0c8813c183ec4afcee5f12604f7f8a
xvanaf005 <vana.frantisek7@seznam.cz>
2023-06-23 13:43:19 +0200

    Přidejte soubory projektu.

---
+++ SMSreciever/Controllers/SmsController.cs
@@ -0,0 +1,21 @@
+﻿// Code sample for ASP.NET MVC on .NET Framework 4.6.1+
+// In Package Manager, run:
+// Install-Package Twilio.AspNet.Mvc -DependencyVersion HighestMinor
+using Twilio.AspNet.Common;
+using Twilio.AspNet.Mvc;
+using Twilio.TwiML;
+
+namespace SMSreciever.Controllers
+{
+    public class SmsController : TwilioController
+    {
+        public TwiMLResult Index(SmsRequest incomingMessage)
+        {
+            var messagingResponse = new MessagingResponse();
+            messagingResponse.Message("The copy cat says: " +
+                                      incomingMessage.Body);
+
+            return TwiML(messagingResponse);
+        }
+    }
+}
---
+++ SMSreciever/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SMSreciever.MvcApplication" Language="C#" %>
